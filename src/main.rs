9284b557dcdd7d7efd87a560e74dea220b5b91c6
Rasmus Lind <rasmus@voiceboxer.com>
2023-04-19 18:34:22 +0200

    added dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// Resources for learning: 
+// https://medium.com/intelliconnect-engineering/uploading-files-to-aws-s3-using-axum-a-rust-framework-c96b1c774dfc
+// https://www.youtube.com/watch?v=DLmyW58egg4
+
 fn main() {
     println!("Hello, world!");
 }
