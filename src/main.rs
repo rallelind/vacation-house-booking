c98cffc08540ff2e88cf9d664c66f3a3821ba056
Rasmus Lind <rasmus@voiceboxer.com>
2023-04-19 19:27:56 +0200

    init project

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@
 // https://medium.com/intelliconnect-engineering/uploading-files-to-aws-s3-using-axum-a-rust-framework-c96b1c774dfc
 // https://www.youtube.com/watch?v=DLmyW58egg4
 
+use axum::{
+    extract::MultiPart,
+    http::StatusCode,
+    routing::{get, post},
+    Extension, Json, Router
+}
+
 fn main() {
     println!("Hello, world!");
 }
