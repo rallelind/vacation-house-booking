747c08e726b467bf8d44e603defaced8e268cd65
Rasmus Lind <rasmus@voiceboxer.com>
2023-04-21 19:12:29 +0200

    started building out server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Resources for learning: 
+// Resources for learning:
 // https://medium.com/intelliconnect-engineering/uploading-files-to-aws-s3-using-axum-a-rust-framework-c96b1c774dfc
 // https://www.youtube.com/watch?v=DLmyW58egg4
 
@@ -6,7 +6,7 @@ use axum::{
     extract::MultiPart,
     http::StatusCode,
     routing::{get, post},
-    Extension, Json, Router
+    Extension, Json, Router,
 };
 
 use std::collections::HashMap;
@@ -15,6 +15,34 @@ use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::Subs
 
 use aws_sdk_s3 as s3;
 
-fn main() {
-    println!("Hello, world!");
+use s3::Client;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG")
+                .unwrap_or_else(|_| "aws-s3-file-upload-api-rust=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let cors_layer = CorsLayer::permissive();
+
+    let aws_configuration = aws_config::load_from_env().await;
+
+    let aws_s3_client = Client::new(&aws_configuration);
+
+    let app = Router::new()
+        .route("/", get(|| async move { "welcome to image upload api" }))
+        .route("/upload", post(upload_image))
+        .layer(cors_layer)
+        .layer(Extension(aws_s3_client));
+
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 3000));
+    tracing::debug!("starting server on port: {}", addr.port());
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("failed to start server");
 }
