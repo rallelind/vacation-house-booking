8c9589c6f644807d7e194b544c6b104adc6f15a3
Rasmus Lind <rasmus@voiceboxer.com>
2023-04-21 20:10:14 +0200

    upload handler (bettter error handling missing)

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 // https://www.youtube.com/watch?v=DLmyW58egg4
 
 use axum::{
-    extract::MultiPart,
+    extract::Multipart,
     http::StatusCode,
     routing::{get, post},
     Extension, Json, Router,
@@ -45,4 +45,63 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .expect("failed to start server");
+}
+
+async fn upload_image(
+    Extension(s3_client): Extension<Client>,
+    mut files: Multipart,
+) -> Result<Json<serde_json::Value>, (StatusCode, Json<serde_json::Value>)> {
+    // get the name of aws bucket from env variable
+    let bucket = std::env::var("AWS_S3_BUCKET").unwrap_or("my-bucket-name".to_owned());
+    // if you have a public url for your bucket, place it as ENV variable BUCKET_URL    
+    //get the public url for aws bucket
+    let bucket_url = std::env::var("BUCKET_URL").unwrap_or(bucket.to_owned())  
+    // we are going to store the respose in HashMap as filename: url => key: value
+    let mut res = HashMap::new();
+    while let Some(file) = files.next_field().await.unwrap() {
+        // this is the name which is sent in formdata from frontend or whoever called the api, i am
+        // using it as category, we can get the filename from file data
+        let category = file.name().unwrap().to_string();
+        // name of the file with extention
+        let name = file.file_name().unwrap().to_string();
+        // file data
+        let data = file.bytes().await.unwrap();
+        // the path of file to store on aws s3 with file name and extention
+        // timestamp_category_filename => 14-12-2022_01:01:01_customer_somecustomer.jpg
+        let key = format!(
+            "images/{}_{}_{}",
+            chrono::Utc::now().format("%d-%m-%Y_%H:%M:%S"),
+            &category,
+            &name
+        );
+
+        // send Putobject request to aws s3
+        let _resp = s3_client
+            .put_object()
+            .bucket(&bucket)
+            .key(&key)
+            .body(data.into())
+            .send()
+            .await
+            .map_err(|err| {
+                dbg!(err);
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    Json(serde_json::json!({"err": "an error occured during image upload"})),
+                )
+            })?;
+        dbg!(_resp);
+        res.insert(
+            // concatinating name and category so even if the filenames are same it will not
+            // conflict
+            format!("{}_{}", &name, &category),
+            format!(
+                "{}/{}",
+                bucket_url,
+                key
+            ),
+        );
+    }
+    // send the urls in response
+    Ok(Json(serde_json::json!(res)))
 }
