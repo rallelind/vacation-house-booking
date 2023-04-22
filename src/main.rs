728a11caafb1f704e0ce038b52c1f1f63aa7de3d
Rasmus Lind <rasmus@voiceboxer.com>
2023-04-22 23:07:52 +0200

    remove comment

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-// Resources for learning:
-// https://medium.com/intelliconnect-engineering/uploading-files-to-aws-s3-using-axum-a-rust-framework-c96b1c774dfc
-// https://www.youtube.com/watch?v=DLmyW58egg4
-
 use axum::{
     routing::{get, post},
     Extension, Router,
