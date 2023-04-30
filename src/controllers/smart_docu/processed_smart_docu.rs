61a6f57b0ea62c3b969a40ac1a5eb39ef2095edf
Rasmus Lind <rasmus@voiceboxer.com>
2023-04-30 20:54:07 +0200

    initiated work on connecting to mongodb

--- src/controllers/smart_docu/processed_smart_docu.rs
+++ src/controllers/smart_docu/processed_smart_docu.rs
@@ -2,6 +2,7 @@ use axum::{extract::Json, http::StatusCode};
 use serde::Deserialize;
 use serde_json::Value;
 
+
 #[derive(Deserialize)]
 pub struct ProcessedSmartDocuBody {
     content: String, // todo figure out best way create a structure for the text content
@@ -10,4 +11,8 @@ pub struct ProcessedSmartDocuBody {
 pub async fn processed_smart_docu(
     Json(payload): Json<ProcessedSmartDocuBody>,
 ) -> Result<Json<Value>, (StatusCode, Json<Value>)> {
+
+    Ok(Json(
+        serde_json::json!(payload.content),
+    ))
 }
