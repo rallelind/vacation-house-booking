3872e64c3df73dc55e32de6bcd284ab86a0b694b
Rasmus Lind <rasmus@voiceboxer.com>
2023-04-30 21:04:09 +0200

    fix

--- src/models/users.rs
+++ src/models/users.rs
@@ -1,7 +1,8 @@
-use serde::{Deserialize, Serialize}
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct User {
     pub email: String,
     pub password: String,
+    pub avatar: String,
 }
