625615f31bb51c031910e6437c640deb8217c7ff
Rasmus Lind <rasmus@voiceboxer.com>
2023-04-30 21:07:52 +0200

    updated model

--- src/models/users.rs
+++ src/models/users.rs
@@ -1,7 +1,10 @@
 use serde::{Deserialize, Serialize};
+use mongodb::bson::oid::ObjectId;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct User {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
     pub email: String,
     pub password: String,
     pub avatar: String,
